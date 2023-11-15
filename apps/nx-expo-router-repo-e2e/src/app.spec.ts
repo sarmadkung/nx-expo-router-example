7b6403b63db2fe80458e27c5fb9cb5817c3c0bfc
Muhammad Sarmad <muhammadsarmad24@gmail.com>
2023-11-15 17:14:15 +0500

    init

--- apps/nx-expo-router-repo-e2e/src/app.spec.ts
+++ apps/nx-expo-router-repo-e2e/src/app.spec.ts
@@ -1,11 +1,13 @@
 import { device, element, by, expect } from 'detox';
 
-describe('Undefined', () => {
+describe('nx-expo-router-repo', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
   it('should display welcome message', async () => {
-    await expect(element(by.id('heading'))).toHaveText('Welcome Undefined 👋');
+    await expect(element(by.id('heading'))).toHaveText(
+      'Welcome nx-expo-router-repo 👋'
+    );
   });
 });
--- apps/nx-expo-router-repo/jest.config.ts
+++ apps/nx-expo-router-repo/jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'undefined',
+  displayName: 'nx-expo-router-repo',
   resolver: '@nx/jest/plugins/resolver',
   preset: 'jest-expo',
   transformIgnorePatterns: [
